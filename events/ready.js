2d4f82fdfb370c7325b1c9d2ed6cc44434642b2f
Lucifer <79420176+Luci1011@users.noreply.github.com>
2021-11-05 13:56:06 +0700

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,27 @@
+const pkg = require("../package.json");
+const axios = require("axios");
+
+module.exports = async (client) => {
+  const version = pkg.version;
+
+  const users = client.users.cache.size;
+  const channels = client.channels.cache.size;
+  const guilds = client.guilds.cache.size;
+
+  console.log(
+    `${client.user.username} đã đăng nhập!`
+  );
+
+  client.setInterval(async () => {
+    let userFormat = client.util.nFormatter(users);
+    let guildFormat = client.util.nFormatter(client.guilds.cache.size);
+
+    let status = [ 
+      'Tú', 
+      'Luci',
+    ];
+    let rand = client.util.getRandInt(status.length);
+
+    client.user.setActivity(status['cùng anh em gay gangz'], { type: "PLAYING" });
+  }, 6e4);
+};
